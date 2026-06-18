a6169f0ac76d7195c2f519afc276292db0f50a5b
WildArio <reperti1900@gmail.com>
2026-06-18 20:08:51 +0400

    Full selection implementation of Cables Motors and Breakers

--- Tether/src/data/selection.cpp
+++ Tether/src/data/selection.cpp
@@ -2,26 +2,27 @@
 #include "equipment.hpp"
 #include "selection.hpp"
 
-static constexpr double BREAKER_COEF{ 1.2 };
-
 namespace Tether::Data {
 
-	std::optional<Breaker> selectBreaker(const std::vector<Breaker>& catalog, double current) {
-		return findNearest(catalog, current,
-			[](const Breaker& b, double val) { return b.nominalCurrent < val; });
+	std::optional<Breaker> selectBreaker(const std::vector<Breaker>& catalog, double current, double heatCoef) {
+		if (catalog.empty()) return std::nullopt;
+
+		auto it{ Detail::findFirstGreaterOrEqual(catalog, current * heatCoef, [](const Breaker& b) { return b.nominalCurrent; }) };
+
+		return (it != catalog.end()) ? *it : *std::prev(it);
 	}
 
 	std::optional<Cable> selectCable(const std::vector<Cable>& catalog, double current) {
-		return findNearest(catalog, current,
-			[](const Cable& c, double val) { 
-				double realCurrent{ c.maxCurrent * BREAKER_COEF };
-				return realCurrent < val;
-			}
-		);
+		if (catalog.empty()) return std::nullopt;
+
+		auto it{ Detail::findFirstGreaterOrEqual(catalog, current, [](const Cable& c) { return c.maxCurrent; }) };
+
+		return (it != catalog.end()) ? *it : *std::prev(it);
 	}
 
 	std::optional<Motor> selectMotor(const std::vector<Motor>& catalog, double power) {
-		return findNearest(catalog, power,
-			[](const Motor& m, double val) { return m.power < val; });
+		if (catalog.empty()) return std::nullopt;
+
+		return Detail::findNearest(catalog, power, [](const Motor& m) { return m.power; });
 	}
 }
